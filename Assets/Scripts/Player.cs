1ce55f7b639c09fd593710a73a3de0d68806d0e3
Haesst <robin.ortryd@futuregames.nu>
2020-03-04 17:37:11 +0100

    Created assignment

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Player : MonoBehaviour
+{
+    CharacterController characterController;
+    Camera mainCamera;
+    ThirdPersonCamera thirdPersonCamera;
+
+    [SerializeField] private float speed = 6.0f;
+    [SerializeField] private float jumpSpeed = 8.0f;
+    [SerializeField] private float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        mainCamera = Camera.main;
+        thirdPersonCamera = mainCamera.GetComponent<ThirdPersonCamera>();
+    }
+
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            SetMoveDirection();
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void SetMoveDirection()
+    {
+        if (thirdPersonCamera && thirdPersonCamera.UseCameraRotation())
+        {
+            SetMoveDirectionUsingCameraRotation();
+        }
+        else
+        {
+            SetMoveDirectionUsingPlayerRotation();
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+            moveDirection.y = jumpSpeed;
+        }
+    }
+
+    private void SetMoveDirectionUsingCameraRotation()
+    {
+        float desiredAngle = mainCamera.transform.eulerAngles.y;
+
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+
+        Vector3 forward = (rotation * Vector3.forward) * Input.GetAxis("Vertical");
+        Vector3 right = (rotation * Vector3.right) * Input.GetAxis("Horizontal");
+
+        moveDirection = forward + right;
+        moveDirection *= speed;
+    }
+
+    private void SetMoveDirectionUsingPlayerRotation()
+    {
+        Quaternion rotation = transform.rotation;
+        Vector3 forward = (rotation * Vector3.right) * Input.GetAxis("Vertical");
+        Vector3 right = (rotation * Vector3.back) * Input.GetAxis("Horizontal");
+
+        moveDirection = forward + right;
+        moveDirection *= speed;
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class ThirdPersonCamera : MonoBehaviour
+{
+    private Vector2 rotation;
+    private float cameraZoomOffsetX;
+    private float cameraZoomOffsetZ;
+
+    [Header("Player Transform")]
+    [SerializeField] private Transform player = null;
+    [Header("General Settings")]
+    [SerializeField] private bool useRightClickToRotateCamera = false;
+    [SerializeField] private bool cameraControlsPlayerRotation = false;
+    [SerializeField] private bool useRightClickToRotatePlayer = false;
+    [Header("Camera rotation")]
+    [SerializeField] private float cameraRotationSpeed = 4.0f;
+    [SerializeField] private float verticalRotationLock = 60.0f;
+    [SerializeField] private Vector3 cameraOffset = new Vector3(6, 0, 6);
+    [Header("Camera zoom")]
+    [SerializeField] private float zoomSpeed = 5.0f;
+    [SerializeField] private float zoomMinDistance = 2.0f;
+    [SerializeField] private float zoomMaxDistance = 8.0f;
+    [Header("Camera Movement")]
+    [Range(0.1f, 0.9f)]
+    [SerializeField] private float cameraMoveLerpSpeed = 0.1f;
+
+    private void Start()
+    {
+        cameraZoomOffsetX = cameraOffset.x;
+        cameraZoomOffsetZ = cameraOffset.z;
+    }
+
+    private void LateUpdate()
+    {
+        if (!useRightClickToRotateCamera || (useRightClickToRotateCamera && Input.GetMouseButton(1)))
+        {
+            SetRotation();
+        }
+
+        SetCameraZoom();
+
+        Vector3 newCameraLocation = GetCameraLocation();
+
+        transform.position = Vector3.Lerp(transform.position, newCameraLocation, cameraMoveLerpSpeed);
+        transform.LookAt(player);
+    }
+
+    private void SetRotation()
+    {
+        rotation.y += Input.GetAxis("Mouse X") * cameraRotationSpeed;
+        rotation.x += -Input.GetAxis("Mouse Y") * cameraRotationSpeed;
+        rotation.x = Mathf.Clamp(rotation.x, -verticalRotationLock, verticalRotationLock);
+    }
+
+    private void SetCameraZoom()
+    {
+        cameraZoomOffsetX += -Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        cameraZoomOffsetZ += -Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+
+        cameraZoomOffsetX = Mathf.Clamp(cameraZoomOffsetX, zoomMinDistance, zoomMaxDistance);
+        cameraZoomOffsetZ = Mathf.Clamp(cameraZoomOffsetZ, zoomMinDistance, zoomMaxDistance);
+
+        cameraOffset = new Vector3(cameraZoomOffsetX, cameraOffset.y, cameraZoomOffsetZ);
+    }
+
+    private Vector3 GetCameraLocation()
+    {
+        if(cameraControlsPlayerRotation || (useRightClickToRotatePlayer && Input.GetMouseButton(1)))
+        {
+            player.rotation = Quaternion.Euler(0, rotation.y, 0);
+        }
+        Vector3 newLocation = player.position - (Quaternion.Euler(rotation.x, rotation.y, 0) * cameraOffset);
+
+        newLocation = CheckForCameraObstruction(newLocation);
+
+        return newLocation;
+    }
+
+    private Vector3 CheckForCameraObstruction(Vector3 cameraLocation)
+    {
+        RaycastHit hit;
+
+        if (Physics.Linecast(player.position, cameraLocation, out hit))
+        {
+            Debug.Log(hit.collider.transform.name);
+            cameraLocation = hit.point;
+        }
+
+        return cameraLocation;
+    }
+
+    public bool UseCameraRotation()
+    {
+        return !useRightClickToRotatePlayer;
+    }
+}
